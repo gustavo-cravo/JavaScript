3b5c1faec2d423ed08569db6cf9c62749af21d05
Gustavo <gustavocravo58@gmail.com>
2026-01-19 16:45:19 -0300

    Add tab switching functionality to main.js
    Implemented event listeners for tab elements to enable switching between content sections. Clicking a tab now activates the corresponding content and updates the active class accordingly.

--- Atividades/DOM/Médio 8/main.js
+++ Atividades/DOM/Médio 8/main.js
@@ -1,1 +1,17 @@
 const tabs = document.querySelectorAll('.tab')
+const content = document.querySelectorAll('.content')
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const tabId = tab.dataset.tab;
+
+        tabs.forEach(t => t.classList.remove('active'));
+
+        content.forEach(t => t.classList.remove('active'));
+
+        tab.classList.add('active')
+
+        document.querySelector(`.content[data-content="${tabId}"]`)
+        .classList.add('active');
+    });
+});
